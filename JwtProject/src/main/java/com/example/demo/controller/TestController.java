bea3cd5681f5a1e2e1a2bb0ec0e009b61e9cddbf
mudassar100 <mudassarnawab01@gmail.com>
2024-06-13 22:20:06 +0530

    by mudassar:fixed mapping request

--- JwtProject/src/main/java/com/example/demo/controller/TestController.java
+++ JwtProject/src/main/java/com/example/demo/controller/TestController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +10,7 @@ public class TestController {
 
     Logger logger = LoggerFactory.getLogger(TestController.class);
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test() {
         this.logger.warn("This is working message");
         return "Testing message";
